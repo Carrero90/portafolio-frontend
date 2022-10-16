0ae07ffb632e9718bb0a4bdebd6ecd8c12ffa381
Cristal Naranjo <cristalnaranjo@gmail.com>
2022-10-16 20:02:52 -0300

    arreglando animacion, loader

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -14,7 +14,7 @@ export const slideInAnimation =
                 })
             ], { optional: true }),
             query(':enter', [
-                style({ left: '-100%' })
+                style({ left: '100%' })
             ], { optional: true }),
             query(':leave', animateChild(), { optional: true }),
             group([
@@ -39,7 +39,7 @@ export const slideInAnimation =
                 })
             ], { optional: true }),
             query(':enter', [
-                style({ left: '-100%' })
+                style({ left: '100%' })
             ], { optional: true }),
             query(':leave', animateChild(), { optional: true }),
             group([
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,8 +38,8 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]   
-    
+    exports: [RouterModule],   
+       
 })
 export class AppRoutingModule {
 
